df443860d4232364afa335728b0b8f49aa3ecac0
Christopher Chew <fishkepr@comcast.net>
2018-01-20 00:23:31 -0800

    Ex3 Final

--- js/app.js
+++ js/app.js
@@ -60,12 +60,23 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var result = sum(a,b);
+  var element1 = sum(result[0],c);
+  console.log("element1: " + element1);
+  result = multiply(a,b);
+  var element2 = multiply(result[0],c);
+  console.log("element2: " + element2);
+  var element3 = a + " and " + b + " and " + c + " sum to " + element1[0] + ".";
+  var element4 = "The product of " + a + " and " + b + " and " + c + " is " + element2[0] + ".";
+  return [element1[0], element2[0], element3, element4];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
-
+testSumAndMultiply(4,7,5);
+result = sumAndMultiply(4,7,5);
+element = document.getElementById('sumAndMultiply1');
+element.textContent = "Result array returned from sumAndMultiply is: " + result;
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
